d669ba4283a09e589e501d5b54c657e185bf6090
JoeBlek <124859655+JoeBlek@users.noreply.github.com>
2024-03-13 18:07:01 -0500

    Update ejercicio.js

--- js/scripts/ejercicio.js
+++ js/scripts/ejercicio.js
@@ -1,3 +1,17 @@
+var loadingBar = 200;
+
+function cambiarTamano(){    
+    var elements = document.getElementsByClassName("loading-bar");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.width=(loadingBar+"px");
+    }
+    loadingBar++;
+    if(loadingBar === 900){
+         clearInterval(intervalId);
+    }
+}
+var intervalId = setInterval(animationBarOne,1);
+
 function cambiarColor(){
     variable_album = document.getElementById("album");
     variable_album.style.backgroundColor = "red";
