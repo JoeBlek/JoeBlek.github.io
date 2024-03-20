000c5eb6bde3cc8ddfb90a596264bdbf0fb4673a
JoeBlek <124859655+JoeBlek@users.noreply.github.com>
2024-03-19 20:08:08 -0500

    Update ejercicio.js

--- js/scripts/ejercicio.js
+++ js/scripts/ejercicio.js
@@ -1,14 +1,4 @@
 
-function cambiarTamano(){
-    var myImg = document.getElementByclass("imagen2");
-       var currWidth = myImg.clientWidth;
-    if(currWidth >= 50){
-           myImg.style.width = (currWidth—100) + "px";
-    }
-}
-
-
-
 function cambiarColor(){
     variable_album = document.getElementById("album");
     variable_album.style.backgroundColor = "red";
