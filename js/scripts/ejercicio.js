9605646a152ad3ce24e17103ca9c4cb4dcb466c5
JoeBlek <124859655+JoeBlek@users.noreply.github.com>
2024-03-19 19:46:34 -0500

    Update ejercicio.js

--- js/scripts/ejercicio.js
+++ js/scripts/ejercicio.js
@@ -8,15 +8,5 @@ function cambiarColor(){
 
 
 
-function cambiarTamano(){    
-    var elements = document.getElementsByClassName("imagen");
-    for (imagen i = 0; i < elements.height; i++) {
-        elements[i].style.width=(imagen+"px");
-    }
-   imagen++;
-    if(imagen === 900){
-         clearInterval(intervalId);
-    }
-}
 
 
