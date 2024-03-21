e06a14d4883d9545cf4f39e0ccf91345bdea3052
JoeBlek <124859655+JoeBlek@users.noreply.github.com>
2024-03-21 10:11:58 -0500

    Update ejercicio.js

--- js/scripts/ejercicio.js
+++ js/scripts/ejercicio.js
@@ -4,9 +4,16 @@ function cambiarColor(){
     variable_album.style.backgroundColor = "red";
 
 }
-
-
   
-
+function cambiarTamano() {
+    // Obtener todos los elementos con la clase 'imagen'
+    var imagenes = document.getElementsByClassName('imagen');
+    // Recorrer cada elemento y cambiar su tamaño
+    for (var i = 0; i < imagenes.length; i++) {
+        // Cambiar el tamaño del elemento (por ejemplo, duplicar su ancho)
+        imagenes[i].style.width = (parseFloat(imagenes[i].style.width || getComputedStyle(imagenes[i]).width) * 2) + 'px';
+        // Puedes ajustar el factor de cambio según tus necesidades
+    }
+}
 
 
